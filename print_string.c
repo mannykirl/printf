266d772e7d2acffa1eeec7e7db64042017a6774f
Lenaice <106773910+Lenaice@users.noreply.github.com>
2022-08-08 19:45:14 -0700

    new code

--- print_string.c
+++ print_string.c
@@ -1,9 +1,22 @@
 #include "main.h"
+
 /**
  * print_string - print a string.
  * @val: argument.
  * Return: the length of the string.
  */
+
+int _strlen(char *s)
+{
+	int len = 0;
+	
+	while(*s != '\0')
+	{
+		s++
+		len++;
+	}
+	return (len);
+}
 
 int print_string(va_list val)
 {
